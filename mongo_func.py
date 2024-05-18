985fa63190c36ffa769f635784d937373c380196
Yeon Lee <82836242+yeonholee50@users.noreply.github.com>
2024-05-18 10:34:11 -0400

    created popualte

--- mongo_func.py
+++ mongo_func.py
@@ -6,7 +6,7 @@ def get_database():
     connection_string = "mongodb+srv://yeon_lee:1234@cluster0.6j64or2.mongodb.net/"
     client = MongoClient(connection_string)
     return client
-def main():
+def populate():
     response = random_people_swap.main()
     client = get_database()
     db = client.sample
@@ -15,6 +15,20 @@ def main():
     result = coll.insert_many([response])
     print(result.inserted_ids)
     client.close()
+
+def find():
+    client = get_database()
+    db = client.sample
+    coll = db.people
+    coll.drop()
+    cursor = coll.find()
+    for doc in cursor:
+        print(doc)
+    client.close() 
+
+def main():
+    populate()
+    find()
     
 
 if __name__ == "__main__":
